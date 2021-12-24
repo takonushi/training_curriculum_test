e593d14fb33bba0788c6c46f6bc67328589efa6d
takonushi <hewitt0120@gmail.com>
2021-12-24 14:05:05 +0900

    Revert "Update calendars_controller.rb"
    This reverts commit 2fe6e117cc25a0c5ce3e7ef383198f32ff343078.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,6 +20,7 @@ class CalendarsController < ApplicationController
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    @todays_wday = 
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
